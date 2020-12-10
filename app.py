795f3638d94ebf49d988b53b744a90957e41d866
zbannor <zbannor@villagemd.com>
2020-12-09 22:02:01 -0600

    tags

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 from pathlib import Path  # python3 only
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 from aws_cdk import core
 from aws_cdk_poc.cdk_airflow_stack import InfutorAirflowStack
+
+
+def _add_tags(stack: core.Stack, more_tags: dict=None)-> None:
+    tags = {
+        'CreatedOn': datetime.now().strftime('%Y-%d-%m'),
+        'CreatedBy': os.getenv('user')
+    }
+
+    if more_tags is not None:
+        tags.update(more_tags)
+
+    for k,v in tags.items():
+        core.Tag.add(stack, k, v)
 
 
 env_path = Path('.') / '.stack_env'
@@ -13,6 +27,8 @@ load_dotenv(dotenv_path=env_path)
 app = core.App()
 
 stack = InfutorAirflowStack(app, id=os.getenv('airflow_stack_name'))
+
+_add_tags(stack)
 
 stack.create_stack()
 
